c028a8281b2dde25823717d8e45feaf4c37b6424
Johnstein <joao.neto.feup@gmail.com>
2015-05-04 13:30:06 +0100

    Android Project
    Alterado o projecto do cliente para Android, têm que instalar o sdk e
    alterar no unity o diretorio

--- SDIS_Client/Assets/www_test.cs
+++ SDIS_Client/Assets/www_test.cs
@@ -3,36 +3,49 @@ using System.Collections;
 using System.Net;
 using System.Text;
 using System.IO;
+using UnityEngine.UI;
 
 public class www_test : MonoBehaviour {
 
 	WWWForm form;
+	Text input;
+    Text log;
 
 	// Use this for initialization
 	void Start () {
 	
+		input = GameObject.FindGameObjectWithTag ("IP").GetComponent<Text>();
+        log = GameObject.FindGameObjectWithTag("Finish").GetComponent<Text>();
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		
+		/*
 		if (Input.GetButton ("Jump")) {
-			HttpWebRequest request = (HttpWebRequest) System.Net.WebRequest.Create("http://127.0.0.1:8000/test/");
-
-			request.Method = "POST";
-
-			string postData = "test one";
-			ASCIIEncoding encoding = new ASCIIEncoding ();
-			byte[] byte1 = encoding.GetBytes (postData);
-			
-			// Set the content length of the string being posted.
-			request.ContentLength = byte1.Length;
-
-			Stream newStream = request.GetRequestStream ();
-			newStream.Write (byte1, 0, byte1.Length);
-			//StartCoroutine(Test());
+			connect();
 		}
+		*/
+	}
+
+	public void connect ()
+	{
+		HttpWebRequest request = (HttpWebRequest) System.Net.WebRequest.Create(input.text);
+		
+		request.Method = "POST";
+		
+		string postData = "test one";
+		ASCIIEncoding encoding = new ASCIIEncoding ();
+		byte[] byte1 = encoding.GetBytes (postData);
+		
+		// Set the content length of the string being posted.
+		request.ContentLength = byte1.Length;
+		
+		Stream newStream = request.GetRequestStream ();
+        newStream.Write(byte1, 0, byte1.Length);
+
+        log.text += "Sent message";
+		//StartCoroutine(Test());
 	}
 
 	IEnumerator Test () {
